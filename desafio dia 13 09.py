d4e09b7badc4662b08972a7091ea6d8aea83fa85
mitch-hannigan <84028789+mitch-hannigan@users.noreply.github.com>
2021-09-13 16:03:29 -0300

    atividade do dia 13 de cetembro.

---
+++ desafio dia 13 09.py
@@ -0,0 +1,4 @@
+# esse programa diz quantas pessoas devem existir em uma sala de aula para que aja 80% de chance de duas pessoas fazerem aniversário no mesmo dia.
+# a, chance está escrito corretamente agora.
+# o código vai ter uma linha, não creio.
+print(f"para que exista 80% de chance de 2 alunos fazerem aniversário no mesmo dia, devem haver {round((365*0.8)*2, 0)} alunos na sala de aula.")
